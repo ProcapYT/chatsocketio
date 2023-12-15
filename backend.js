905d2146a0d2563463ce15ee1bcaf4cfeef49542
ProcapYT <samuelpedrera7@gmail.com>
2023-12-15 16:19:11 +0100

    added app

---
+++ backend.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const http = require('http')
+const { Server } = require('socket.io')
+const path = require('path')
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
+
+const port = process.env.PORT || 3000
+
+const yellow = '\x1b[33m'
+const bold = '\x1b[1m'
+const endColor = '\x1b[0m'
+
+const users = {}
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+io.on('connection', socket => {
+    console.log('New user connected', socket.id)
+
+    socket.on('disconnect', reason => {
+        console.log('A user has disconnected:', socket.id, '| for:', reason)
+        for (const [username, id] of Object.entries(users)) {
+            if (id === socket.id) {
+                delete users[username];
+                break;
+            }
+        }
+
+        io.emit('userList', Object.keys(users))
+    })
+
+    socket.on('newMessage', data => {
+        const { user, message } = data
+
+        io.emit('message', { user, message })
+    })
+
+    socket.on('newUser', inputValue => {
+        if (users[inputValue]) {
+            socket.emit('usernameError', 'The username is already in use')
+        } else {
+            users[inputValue] = socket.id
+            io.emit('userList', Object.keys(users))
+        }
+    })
+
+    socket.on('privateMessage', data => {
+        const { toUser, privateMessage } = data;
+        const fromUser = Object.keys(users).find(key => users[key] === socket.id);
+
+        if (users[toUser]) {
+            io.to(users[toUser]).emit('privateMessage', { fromUser, privateMessage });
+        } else {
+            socket.emit('privateMessageError', 'User not found');
+        }
+    });
+})
+
+server.listen(port, () => {
+    console.log(`Enter ${bold + yellow}http://localhost:${port}${endColor}`)
+})
---
+++ public/frontend.js
@@ -0,0 +1,94 @@
+const socket = io()
+
+const inputMessage = document.querySelector('#input')
+const messagesBox = document.querySelector('#messagesBox')
+const sendButton = document.querySelector('#sendButton')
+const mainChat = document.querySelector('#mainChat')
+const usernameInput = document.querySelector('#usernameInput')
+const usernameButton = document.querySelector('#usernameButton')
+const userNames = document.querySelector('#userNames')
+const usernameErrorContainer = document.querySelector('#usernameErrorContainer')
+const usernameMain = document.querySelector('#usernameMain')
+
+let cleanCurrentUser = null
+
+mainChat.style.display = 'none'
+usernameErrorContainer.style.display = 'none'
+
+const sendMessage = () => {
+    if (inputMessage.value) {
+        const message = inputMessage.value
+        if (message.startsWith('/w ')) {
+            const parts = message.split(' ')
+            const toUser = parts[1]
+            const privateMessage = parts.slice(2).join(' ')
+
+            socket.emit('privateMessage', { toUser, privateMessage })
+        } else {
+            socket.emit('newMessage', { user: cleanCurrentUser, message })
+        }
+
+        inputMessage.value = ''
+    }
+}
+
+const addUsername = () => {
+    if (usernameInput.value) {
+        const currentUser = usernameInput.value
+        cleanCurrentUser = currentUser.replace(/\s/g, '').toLowerCase() // Convertir a minúsculas
+        socket.emit('newUser', cleanCurrentUser)
+        usernameInput.value = ''
+    }
+}
+
+addEventListener('keydown', e => {
+    if (e.key === 'Enter') {
+        sendMessage()
+        addUsername()
+    }
+})
+
+sendButton.addEventListener('click', () => {
+    sendMessage()
+})
+
+usernameButton.addEventListener('click', () => {
+    addUsername()
+})
+
+socket.on('message', data => {
+    const { user, message } = data
+
+    messagesBox.innerHTML += `<p class="message"><b>${user}:</b> ${message}</p>`
+})
+
+socket.on('userList', users => {
+    userNames.innerHTML = users.map(username => `<p><i class="fa-solid fa-user"></i>&nbsp;${username}</p>`).join('')
+    usernameErrorContainer.innerHTML = ''
+    usernameErrorContainer.style.display = 'none'
+
+    if (cleanCurrentUser) {
+        if (users.includes(cleanCurrentUser)) {
+            usernameMain.style.display = 'none'
+            mainChat.style.display = 'initial'
+        } else {
+            usernameMain.style.display = 'initial'
+            mainChat.style.display = 'none'
+        }
+    }
+})
+
+socket.on('usernameError', errorMsg => {
+    usernameErrorContainer.style.display = 'block'
+    usernameErrorContainer.innerHTML = `<p>${errorMsg}</p>`
+})
+
+socket.on('privateMessageError', errorMsg => {
+    messagesBox.innerHTML += `<p style="color: red;">${errorMsg}</p>`
+})
+
+socket.on('privateMessage', data => {
+    const { fromUser, privateMessage } = data;
+
+    messagesBox.innerHTML += `<p class="message" style="color: rgb(100, 100, 100); font-style: italic;"><b>${fromUser}:</b> ${privateMessage}</p>`;
+});
