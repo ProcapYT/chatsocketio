1dfa7d4fd78aa0a0682ceb5ef55ac59529928894
ProcapYT <samuelpedrera7@gmail.com>
2023-12-19 19:00:40 +0100

    added rooms

--- backend.js
+++ backend.js
@@ -1,31 +1,60 @@
+// Import the necesary dependencies
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const path = require("path");
 
+// Create an express, http and socket.io server
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+// Port variable for the server
 const port = process.env.PORT || 3000;
 
+// Colors for the terminal
 const yellow = "\x1b[33m";
 const bold = "\x1b[1m";
 const endColor = "\x1b[0m";
 
+// User and room hashes
 const users = {};
+const rooms = {};
 
+// Use the public folder as a static site
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+// Check when the frontend connects
 io.on("connection", (socket) => {
   console.log("New user connected", socket.id);
 
+  // Check when the frontend disconnects
   socket.on("disconnect", (reason) => {
     console.log("A user has disconnected:", socket.id, "| for:", reason);
+
+    // Emit the users of each room
+    for (const [room, usersInRoom] of Object.entries(rooms)) {
+      const userIndex = usersInRoom.indexOf(socket.id);
+      if (userIndex !== -1) {
+        usersInRoom.splice(userIndex, 1);
+        io.to(room).emit(
+          "userList",
+          usersInRoom.map((userId) =>
+            Object.keys(users).find((key) => users[key] === userId)
+          )
+        );
+        if (usersInRoom.length === 0) {
+          delete rooms[room];
+          io.emit("roomList", Object.keys(rooms));
+        }
+      }
+    }
+
+    // Delete users when they logout
     for (const [username, id] of Object.entries(users)) {
       if (id === socket.id) {
         delete users[username];
@@ -36,45 +65,113 @@ io.on("connection", (socket) => {
     io.emit("userList", Object.keys(users));
   });
 
+  // Messages logic
   socket.on("newMessage", (data) => {
-    const { user, message } = data;
-    io.emit("message", { user, message });
+    const { user, message, room } = data;
+    io.to(room).emit("message", { user, message });
   });
 
+  // User logic
   socket.on("newUser", (inputValue) => {
     if (users[inputValue]) {
       socket.emit("usernameError", "The username is already in use");
+    } else if (inputValue.length >= 10) {
+      socket.emit("usernameError", "The username is too long");
     } else {
       users[inputValue] = socket.id;
-      io.emit("userList", Object.keys(users));
+      io.emit("user", Object.keys(users));
     }
   });
 
+  // Private message logic
   socket.on("privateMessage", (data) => {
-    const { toUser, privateMessage } = data;
+    const { toUser, privateMessage, room } = data;
     const fromUser = Object.keys(users).find((key) => users[key] === socket.id);
-  
+
     if (fromUser === toUser) {
-      // Si el remitente es el mismo que el destinatario
-      socket.emit("privateMessageError", "You can't send yourself a private message");
+      socket.emit(
+        "privateMessageError",
+        "You can't send yourself a private message"
+      );
     } else if (users[toUser]) {
       const toUserId = users[toUser];
-  
-      // Emitir el mensaje privado al destinatario
+
       io.to(toUserId).emit("privateMessage", {
         fromUser,
         toUser,
         privateMessage,
       });
-  
-      // También enviar el mensaje al remitente
+
       socket.emit("privateMessage", { fromUser, toUser, privateMessage });
     } else {
       socket.emit("privateMessageError", "User not found");
     }
   });
+
+  // Create room logic
+  socket.on("createRoom", (data) => {
+    const { user, room } = data;
+    const cleanRoomName = room.replace(/\s/g, "");
+
+    if (!rooms[cleanRoomName]) {
+      rooms[cleanRoomName] = [socket.id];
+      io.emit("roomList", Object.keys(rooms));
+      io.to(socket.id).emit("joinRoom", { user, room: cleanRoomName });
+      socket.join(cleanRoomName);
+    }
+  });
+
+  // Join room logic
+  socket.on("joinRoom", (data) => {
+    const { user, room } = data;
+    const cleanRoomName = room.replace(/\s/g, "");
+
+    for (const [prevRoom, prevUsersInRoom] of Object.entries(rooms)) {
+      const prevUserIndex = prevUsersInRoom.indexOf(socket.id);
+      if (prevUserIndex !== -1) {
+        prevUsersInRoom.splice(prevUserIndex, 1);
+        io.to(prevRoom).emit(
+          "userList",
+          prevUsersInRoom.map((userId) =>
+            Object.keys(users).find((key) => users[key] === userId)
+          )
+        );
+        if (prevUsersInRoom.length === 0) {
+          delete rooms[prevRoom];
+        }
+        break;
+      }
+    }
+
+    // Detects when a user connects into a room
+    if (rooms[cleanRoomName] && !rooms[cleanRoomName].includes(socket.id)) {
+      rooms[cleanRoomName].push(socket.id);
+      io.to(cleanRoomName).emit("message", {
+        user: "System",
+        message: `${user} has joined the room.`,
+      });
+      socket.join(cleanRoomName);
+      io.to(socket.id).emit("joinRoom", { user, room: cleanRoomName });
+      io.to(cleanRoomName).emit("userList", rooms[cleanRoomName]);
+    }
+  });
+
+  // Each 15 miliseconds emits the room and user list
+  setInterval(() => {
+    for (const room in rooms) {
+      if (rooms.hasOwnProperty(room)) {
+        if (!rooms[room]) {
+          delete rooms[room];
+          io.emit("roomList", Object.keys(rooms));
+        }
+      }
+    }
+    io.emit("userList", Object.keys(users));
+    socket.emit("roomList", Object.keys(rooms));
+  }, 15);
 });
 
+// Listen in the port variable
 server.listen(port, () => {
   console.log(`Enter ${bold + yellow}http://localhost:${port}${endColor}`);
 });
--- public/frontend.js
+++ public/frontend.js
@@ -1,60 +1,166 @@
+// Use socket.io on the frontend
 const socket = io();
 
+// Import HTML elements
 const inputMessage = document.querySelector("#input");
 const messagesBox = document.querySelector("#messagesBox");
 const sendButton = document.querySelector("#sendButton");
 const mainChat = document.querySelector("#mainChat");
 const usernameInput = document.querySelector("#usernameInput");
 const usernameButton = document.querySelector("#usernameButton");
+const createRoomButton = document.querySelector("#createRoomButton");
+const newRoomInput = document.querySelector("#newRoomInput");
+const roomList = document.querySelector("#roomList");
 const userNames = document.querySelector("#userNames");
 const usernameErrorContainer = document.querySelector(
   "#usernameErrorContainer"
 );
 const usernameMain = document.querySelector("#usernameMain");
+const messagesContainer = document.querySelector("#messagesContainer");
 
+// Create variables outside the function
 let cleanCurrentUser = null;
+let currentRoom;
 
-mainChat.style.display = "none";
-usernameErrorContainer.style.display = "none";
+// Hide the mainchat and the error container
+mainChat.classList.add("hidden");
+usernameErrorContainer.classList.add("hidden");
 
+// Create a function to send a message
 const sendMessage = () => {
+  // Check if the input has a value
   if (inputMessage.value) {
+    // Private message logic
     const message = inputMessage.value;
     if (message.startsWith("/w ")) {
       const parts = message.split(" ");
       const toUser = parts[1];
       const privateMessage = parts.slice(2).join(" ");
 
+      // Emit private message to the backend
       socket.emit("privateMessage", { toUser, privateMessage });
     } else {
-      socket.emit("newMessage", { user: cleanCurrentUser, message });
+      // Check if the message isn't /clear
+      if (inputMessage.value.trim().toLowerCase() !== "/clear") {
+        // Emit a new message to the backend
+        socket.emit("newMessage", {
+          user: cleanCurrentUser,
+          message,
+          room: currentRoom,
+        });
+      }
     }
 
+    // Set the input value to a empty string
     inputMessage.value = "";
+
+    // Check if the user is in a room
+    if (!currentRoom && !message.startsWith("/w ")) {
+      messagesBox.innerHTML += `<p style="color: red;">You need to join a room first.</p>`;
+    }
+
+    // Check if the message is /clear to clear the chat
+    if (message.trim().toLowerCase() === "/clear") {
+      messagesBox.innerHTML = "";
+    }
+
+    // Scroll to the top when the user sends a message
+    messagesContainer.scrollTop = messagesContainer.scrollHeight;
   }
 };
 
+// Function to add a username to the backend's hash
 const addUsername = () => {
+  // Check if the value of the input isn't empty
   if (usernameInput.value) {
+    // Create a clean user variable
     const currentUser = usernameInput.value;
-    cleanCurrentUser = currentUser.replace(/\s/g, "");
+    cleanCurrentUser = currentUser.replace(/ /g, "");
+
+    // Emit a new user event to the backend
     socket.emit("newUser", cleanCurrentUser);
+
+    // Clean the input value
     usernameInput.value = "";
+
+    // Set the title to the user
+    document.querySelector(
+      ".roomTitleChat"
+    ).innerText = `Chat - ${cleanCurrentUser}`;
   }
 };
 
-Notification.requestPermission()
+// Create a create room function
+const createRoom = () => {
+  // Get a clean room name
+  const newRoomName = newRoomInput.value.trim();
 
-const showNotification = ({message, user}) => {
-  const notification = new Notification(user, {
-    body: message
-  })
+  // Check if the room name exists
+  if (newRoomName) {
+    // Check if the room name is less then 10 characters
+    if (newRoomName.length <= 10) {
+      // Emit a create room event to the backend
+      socket.emit("createRoom", { user: cleanCurrentUser, room: newRoomName });
 
-  notification.onclick = () => {
-    open('/')
+      // Clean the input value
+      newRoomInput.value = "";
+    }
   }
-}
+};
 
+// Request permission to send notifications
+Notification.requestPermission();
+
+// Create a show notification function
+const showNotification = ({ message, user }) => {
+  // Create a notification
+  const notification = new Notification(user, {
+    body: message,
+  });
+
+  // Check when the notification has been clicked
+  notification.onclick = () => {
+    open("/");
+  };
+};
+
+// Create an update room list function
+const updateRoomList = (rooms) => {
+  // Get the room list element from the HTML
+  const roomListElement = document.querySelector("#roomList");
+  roomListElement.innerHTML = "";
+
+  // Create a for loop to parse the rooms array
+  for (const room in rooms) {
+    if (rooms.hasOwnProperty(room)) {
+      // Create the room button element, give it a class and giving it a dataset
+      const roomButton = document.createElement("button");
+      roomButton.className = "joinRoomButton";
+      roomButton.dataset.room = rooms[room];
+
+      // Create the fontawesome icon element
+      const icon = document.createElement("i");
+      icon.className = "fa-solid fa-door-open";
+      roomButton.appendChild(icon);
+
+      // Create a child of the button with a text(name of the room)
+      const textNode = document.createTextNode(` ${rooms[room]}`);
+      roomButton.appendChild(textNode);
+
+      // Check for when the button is clicked
+      roomButton.addEventListener("click", () => {
+        // Get the room name with the dataset of the button and emit a join room message to the backend
+        const roomName = roomButton.dataset.room;
+        socket.emit("joinRoom", { user: cleanCurrentUser, room: roomName });
+      });
+
+      // Create a child with the button to the room list
+      roomListElement.appendChild(roomButton);
+    }
+  }
+};
+
+// Check when the enter key is pressed and execute the send message and add username functions
 addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
     sendMessage();
@@ -62,43 +168,98 @@ addEventListener("keydown", (e) => {
   }
 });
 
+// Execute the send message function when the send button is clicked
 sendButton.addEventListener("click", () => {
   sendMessage();
 });
 
+// Execute the add username function when the username button is clicked
 usernameButton.addEventListener("click", () => {
   addUsername();
 });
 
-socket.on("userList", (users) => {
-  userNames.innerHTML = users
-    .map(
-      (username) => `<p><i class="fa-solid fa-user"></i>&nbsp;${username}</p>`
-    )
-    .join("");
-  usernameErrorContainer.innerHTML = "";
-  usernameErrorContainer.style.display = "none";
+// Execute the create room function when the create room button is clicked
+createRoomButton.addEventListener("click", () => {
+  createRoom();
+});
 
-  if (cleanCurrentUser) {
-    if (users.includes(cleanCurrentUser)) {
-      usernameMain.style.display = "none";
-      mainChat.style.display = "initial";
-    } else {
-      usernameMain.style.display = "initial";
-      mainChat.style.display = "none";
+// Check if the backend has sent a userlist event
+socket.on("userList", (users) => {
+  // Get the usernames container from the HTML
+  const userNamesContainer = document.querySelector("#userNames");
+
+  // Check if the users array is empty
+  if (users.length === 0) {
+    // Hide the usernames container
+    while (userNamesContainer.firstChild) {
+      userNamesContainer.removeChild(userNamesContainer.firstChild);
     }
+    userNamesContainer.classList.add("hidden");
+  } else {
+    // Clear existing content
+    while (userNamesContainer.firstChild) {
+      userNamesContainer.removeChild(userNamesContainer.firstChild);
+    }
+
+    // For each user create a p tag with the user and a user icon from fontawesome
+    users.forEach((username) => {
+      const paragraphElement = document.createElement("p");
+      const iconElement = document.createElement("i");
+      iconElement.classList.add("fa-solid", "fa-user");
+      const textNode = document.createTextNode(` ${username}`);
+      paragraphElement.appendChild(iconElement);
+      paragraphElement.appendChild(textNode);
+      userNamesContainer.appendChild(paragraphElement);
+    });
+
+    // Unhide the usernames container
+    userNamesContainer.classList.remove("hidden");
   }
 });
 
+// Check if the backend has sent an user event
+socket.on("user", (users) => {
+  // Check if the users array includes the clean user and hide and unhide a bunch of things
+  if (users.includes(cleanCurrentUser)) {
+    usernameMain.classList.add("hidden");
+    mainChat.classList.remove("hidden");
+  } else {
+    usernameMain.classList.remove("hidden");
+    mainChat.classList.add("hidden");
+  }
+});
+
+// Check if the backend has sent an username error event
 socket.on("usernameError", (errorMsg) => {
-  usernameErrorContainer.style.display = "block";
+  // Unhide the username error container and show the error message
+  usernameErrorContainer.classList.remove("hidden");
+  mainChat.classList.add("hidden");
+  usernameMain.classList.remove("hidden");
   usernameErrorContainer.innerHTML = `<p>${errorMsg}</p>`;
 });
 
+// Check if the backend has sent a room list event
+socket.on("roomList", (rooms) => {
+  // Execute the update rooms function with the rooms array
+  updateRoomList(rooms);
+});
+
+// Check if the backend has sent a join room event
+socket.on("joinRoom", (data) => {
+  // Get the user and the room
+  const { user, room } = data;
+  currentRoom = room;
+  document.querySelector(
+    ".roomTitleChat"
+  ).innerText = `Chat (${room}) - ${user}`;
+});
+
+// Check if the backend has sent an error message from the private message and showit
 socket.on("privateMessageError", (errorMsg) => {
   messagesBox.innerHTML += `<p style="color: red;">${errorMsg}</p>`;
 });
 
+// Recive the message event from the backend
 socket.on("message", (data) => {
   const { user, message } = data;
 
@@ -108,22 +269,23 @@ socket.on("message", (data) => {
 
   messagesBox.innerHTML += `<p class="message"><b>${displayName}:</b> ${message}</p>`;
 
-  if(!isCurrentUser) {
-    showNotification({message, user: `${displayName}:`})
+  if (!isCurrentUser) {
+    showNotification({ message, user: `${displayName}:` });
   }
 });
 
-socket.on('privateMessage', data => {
+// Recive the private message event from the backend
+socket.on("privateMessage", (data) => {
   const { fromUser, toUser, privateMessage } = data;
   const isCurrentUser = fromUser === cleanCurrentUser;
 
-  let displayMessage = '';
+  let displayMessage = "";
 
   if (isCurrentUser) {
-      displayMessage = `<p class="message" style="color: rgb(100, 100, 100); font-style: italic;"><b>You to ${toUser}:</b> ${privateMessage}</p>`;
+    displayMessage = `<p class="message" style="color: rgb(100, 100, 100); font-style: italic;"><b>You to ${toUser}:</b> ${privateMessage}</p>`;
   } else if (toUser === cleanCurrentUser) {
-      displayMessage = `<p class="message" style="color: rgb(100, 100, 100); font-style: italic;"><b>${fromUser} to You:</b> ${privateMessage}</p>`;
-      showNotification({message: privateMessage, user: `${fromUser} to You:`})
+    displayMessage = `<p class="message" style="color: rgb(100, 100, 100); font-style: italic;"><b>${fromUser} to You:</b> ${privateMessage}</p>`;
+    showNotification({ message: privateMessage, user: `${fromUser} to You:` });
   }
 
   messagesBox.innerHTML += displayMessage;
